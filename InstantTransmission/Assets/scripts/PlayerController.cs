aae4e4d571b67198be93b7f496ebf24f746da2ee
ibrow19 <innes.brown19@gmail.com>
2018-01-27 08:25:52 +0000

    player control with gamepad

--- InstantTransmission/Assets/scripts/PlayerController.cs
+++ InstantTransmission/Assets/scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour {
 
 	private const float VELOCITY = 1f;
 
-	public Transform mTarget;
+	public string xAxis;
+	public string yAxis;
+
+	public Transform target;
 
 	// Use this for initialization
 	void Start () {
@@ -16,14 +19,14 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		float x = Input.GetAxis("Horizontal");
-		float y = Input.GetAxis("Vertical");
+		float x = Input.GetAxis(xAxis);
+		float y = Input.GetAxis(yAxis);
 
 		Vector2 move = new Vector2(x, y);	
 		move = Vector2.ClampMagnitude (move, VELOCITY * Time.deltaTime);
 		transform.Translate(move.x, move.y, 0, Space.World);
 
-		Vector3 toTarget = mTarget.transform.position - transform.position;
+		Vector3 toTarget = target.transform.position - transform.position;
 		toTarget.Normalize();
 
 		float rotation = Mathf.Atan2(toTarget.y, toTarget.x) * Mathf.Rad2Deg;
