2d69e80ee8b366f27c805af2b7fdc3c1b01c1a91
ibrow19 <innes.brown19@gmail.com>
2018-01-27 13:29:55 +0000

    Attack collision

---
+++ InstantTransmission/Assets/scripts/Attack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+	private bool active = false;
+
+	private PlayerController target;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//void OnTriggerEnter2D(Collider2D other) {
+
+	//	if (other.gameObject == target.gameObject && active) {
+	//		Debug.Log ("Start");
+	//	}
+
+	//}
+
+	void OnTriggerStay2D(Collider2D other) {
+
+		if (other.gameObject == target.gameObject && active) {
+			Debug.Log ("Stay");
+		}
+
+	}
+
+	public void setActive(bool active) {
+		Debug.Log ("Attacking");
+		this.active = active;
+	}
+
+	public void setTarget(PlayerController target) {
+		this.target = target;
+	}
+}
--- InstantTransmission/Assets/scripts/PlayerController.cs
+++ InstantTransmission/Assets/scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour {
 	private const float teleportDistance = 1f;
 	private Vector2 offset = new Vector3 (0f, 1f, 0f);
 
+	private Attack attack;
+	private Animator animator;
+
 	public string xAxis;
 	public string yAxis;
 	public string teleportAxis;
@@ -17,7 +20,9 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		animator = GetComponent<Animator>();
+		attack = transform.Find ("Attack").GetComponent<Attack> ();
+		attack.setTarget (target);
 	}
 	
 	// Update is called once per frame
@@ -31,6 +36,8 @@ public class PlayerController : MonoBehaviour {
 
 		if (Input.GetAxisRaw (teleportAxis) != 0) {
 			teleport (direction);
+		} else if (Input.GetAxisRaw (attackAxis) != 0) {
+			attack.setActive (true);
 		} else {
 			move (direction);
 		}
